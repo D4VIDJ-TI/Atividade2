25551bf349773acfa4c6e9dc4ab7519e0baeba7a
David Jusselino <davidjusselino@gmail.com>
2025-04-21 21:00:42 -0300

    Meu banco de dados

--- src/ConexaoDAO.java
+++ src/ConexaoDAO.java
@@ -1,33 +1,21 @@
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import javax.swing.JOptionPane;
 
+public class ConexaoDAO {
 
-
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Adm
- */
-public class conectaDAO {
-    
-    public Connection connectDB(){
-        Connection conn = null;
-        
+    public Connection connectDB() {
         try {
-        
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
+           String url = "jdbc:mysql://localhost:3306/uc11?useSSL=false";
+            String user = "root"; 
+            String password = "Apolo2010"; 
+
             
-        } catch (SQLException erro){
-            JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
+            Connection conn = DriverManager.getConnection(url, user, password);
+            return conn;
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro na conexão com o banco de dados: " + e.getMessage());
         }
-        return conn;
     }
-    
 }
